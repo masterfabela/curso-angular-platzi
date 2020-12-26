1d73be1076a5eb9ba3140e1bfd2bf6013d69251d
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-26 12:40:03 +0100

    Clase 13

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
