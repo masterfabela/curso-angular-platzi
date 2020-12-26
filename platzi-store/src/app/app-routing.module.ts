395640304b62e658d31e5c32a56aa43fc3f1f54b
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-26 18:03:41 +0100

    Clase 25

--- platzi-store/src/app/app-routing.module.ts
+++ platzi-store/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path: 'products',
     component: ProductsComponent,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
   },
   {
     path: 'contact',
