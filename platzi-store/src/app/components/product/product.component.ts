56f4f3d5cc39e6f2d3d176a56b2d894ff314b225
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-26 13:56:53 +0100

    Clase 16

--- platzi-store/src/app/components/product/product.component.ts
+++ platzi-store/src/app/components/product/product.component.ts
@@ -14,6 +14,7 @@ import { Product } from 'src/app/interfaces/product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit, DoCheck, OnDestroy {
   @Input() product: Product;
