e30cb637d34a8a98bf072be0f14108b10d36b65b
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-26 16:17:27 +0100

    Clase 20

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -7,9 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { ProductComponent } from './components/product/product.component';
 import { CardComponent } from './components/card/card.component';
 import { ExponentialPipe } from './pipes/exponential.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CardComponent, ExponentialPipe],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CardComponent,
+    ExponentialPipe,
+    HighlightDirective,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
