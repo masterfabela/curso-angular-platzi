24c07a28a9403e43281c886b92693d0a2de96a10
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-26 19:16:54 +0100

    Clase 28

--- platzi-store/src/app/app-routing.module.ts
+++ platzi-store/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './modules/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductsComponent } from './components/products/products.component';
---
+++ platzi-store/src/app/modules/home/home.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent, BannerComponent],
+  imports: [HomeRoutingModule, CommonModule],
+})
+export class HomeModule {}
