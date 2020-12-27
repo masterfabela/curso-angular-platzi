226cb4762ce2b8274d8e472c721a690e5fdbd39a
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-27 23:27:02 +0100

    Clase 47

--- platzi-store/src/app/routes/admin/components/form-product/form-product.component.ts
+++ platzi-store/src/app/routes/admin/components/form-product/form-product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductsService } from 'src/app/core/services/products.service';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-form-product',
@@ -38,7 +39,7 @@ export class FormProductComponent implements OnInit {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price: ['', [Validators.required]],
+      price: ['', [Validators.required, MyValidators.isPriceValid]],
       image: '',
       description: ['', [Validators.required]],
     });
