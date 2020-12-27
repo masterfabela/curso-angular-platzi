a157e9703520ec3799fa9442adc180a502e94bb4
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-27 21:18:07 +0100

    Clase 46

--- platzi-store/src/app/routes/admin/components/form-product/form-product.component.ts
+++ platzi-store/src/app/routes/admin/components/form-product/form-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'app-form-product',
@@ -9,18 +11,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormProductComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private produtsService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm();
   }
 
   ngOnInit(): void {}
+
+  saveProduct(event: Event): void {
+    event.preventDefault();
+    console.log('Producto subido');
+    if (this.form.valid) {
+      console.log('Producto subido');
+      const product = this.form.value;
+      this.produtsService.createProduct(product).subscribe(() => {
+        this.router.navigate(['/admin/products']);
+        console.log('Producto subido');
+      });
+    }
+  }
 
   private buildForm(): void {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: ['', [Validators.required]],
-      image: ['', []],
+      image: '',
       description: ['', [Validators.required]],
     });
   }
