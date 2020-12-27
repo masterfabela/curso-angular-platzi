3cf90b3fd4d3c8a5899602b944e93428d181365c
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2020-12-27 11:10:23 +0100

    Clase 32

--- platzi-store/src/app/routes/products/products.module.ts
+++ platzi-store/src/app/routes/products/products.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from 'src/app/material/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductComponent } from './components/product/product.component';
@@ -9,6 +10,12 @@ import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [ProductComponent, ProductsComponent, ProductDetailComponent],
-  imports: [CommonModule, RouterModule, ProductsRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ProductsRoutingModule,
+    SharedModule,
+    MaterialModule,
+  ],
 })
 export class ProductsModule {}
